4cfc000db0feabe55c63c32ef0738649df11728b
hsukiasyan <h.sukiasyan@gethatch.com>
2023-01-09 09:50:25 +0100

    initial ui

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import './normale.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <aside className='sidemenu'>
+        <div className='aside-menu-button'>
+          <span>+</span>New Chat
+        </div>
+      </aside>
+      <secttio className='chatbox'></secttio>
+    </div>
+  );
+}
+
+export default App;
